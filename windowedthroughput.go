be3a3ac1899eba9f1fcf6350caa65a234399d323
Yi Zhao <yizhao@stripe.com>
2023-10-12 14:35:00 -0400

    Remove math.floor from sampling

--- windowedthroughput.go
+++ windowedthroughput.go
@@ -161,8 +161,8 @@ func (t *WindowedThroughput) updateMaps() {
 	}
 	// figure out our target throughput per key over the lookback window.
 	totalGoalThroughput := t.GoalThroughputPerSec * t.LookbackFrequencyDuration.Seconds()
-	// floor the throughput but min should be 1 event per bucket per time period
-	throughputPerKey := math.Max(1, float64(totalGoalThroughput)/float64(t.numKeys))
+	// split the total throughput equally across the number of keys.
+	throughputPerKey := float64(totalGoalThroughput) / float64(t.numKeys)
 	// for each key, calculate sample rate by dividing counted events by the
 	// desired number of events
 	newSavedSampleRates := make(map[string]int)
